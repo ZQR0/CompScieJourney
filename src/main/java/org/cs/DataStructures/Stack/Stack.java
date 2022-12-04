17758556a7794abcedde27902ed0e4c77470ac22
ZQR0 <yarikrech@gmail.com>
2022-12-04 14:44:19 +0300

    stack documentation

--- src/main/java/org/cs/DataStructures/Stack/Stack.java
+++ src/main/java/org/cs/DataStructures/Stack/Stack.java
@@ -2,8 +2,18 @@ package org.cs.DataStructures.Stack;
 
 import java.util.Vector;
 
+/*
+* @author ZQR0
+* @version 0.0.1
+* */
 public class Stack<T extends Comparable<T>> extends Vector<T> implements Stackable<T> {
+    /*
+    * stack field
+    * */
     private T[] _stack;
+    /*
+    * size of stack (length)
+    * */
     private int _size;
 
     public Stack(T[] data) {
@@ -11,20 +21,27 @@ public class Stack<T extends Comparable<T>> extends Vector<T> implements Stackab
         _size = _stack.length;
     }
 
-    public T[] getStack() {
-        return _stack;
-    }
-
+    /*
+    * @returns null
+    * adds new elem to stack
+    * @params T value
+    * */
     @Override
     public void push(T value) {
         _stack[0] = value;
     }
 
+    /*
+    * @returns boolean value
+    * */
     @Override
     public boolean isEmpty() {
         return _size == 0;
     }
 
+    /*
+    * @returns removed object from top of the stack
+    * */
     @Override
     public synchronized T pop() {
         T result = peek();
@@ -33,17 +50,27 @@ public class Stack<T extends Comparable<T>> extends Vector<T> implements Stackab
         return result;
     }
 
+    /*
+    * @returns object from top of the stack
+    * */
     @Override
     public synchronized T peek() {
         assert _size != 0;
         return _stack[_size - 1];
     }
 
+    /*
+    * @returns size of stack (length)
+    * */
     @Override
     public int size() {
         return _size;
     }
 
+    /*
+    * @returns index of param object
+    * @params Object o
+    * */
     @Override
     public synchronized int search(Object o) {
         int i = lastIndexOf(o);
@@ -54,6 +81,9 @@ public class Stack<T extends Comparable<T>> extends Vector<T> implements Stackab
         return -1;
     }
 
+    /*
+    * private method to remove elem by index
+    * */
     private void removeElem(int index) {
         int j = _size - index - 1;
         if (j > 0) {
