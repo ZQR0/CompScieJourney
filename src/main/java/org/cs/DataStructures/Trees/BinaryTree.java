183a3cf6fed945519a0b480729bbbda0b96ab72c
ZQR0 <yarikrech@gmail.com>
2022-12-09 21:38:39 +0300

    Node data type class created

--- src/main/java/org/cs/DataStructures/Trees/BinaryTree.java
+++ src/main/java/org/cs/DataStructures/Trees/BinaryTree.java
@@ -1,6 +1,30 @@
 package org.cs.DataStructures.Trees;
 
-public class BinaryTree<E> {
+public class BinaryTree {
+
+    static class Node {
+        public int data;
+        public Node left;
+        public Node right;
+        public Node parent;
+
+        public Node(int value) {
+            left = null;
+            right = null;
+            parent = null;
+            data = value;
+        }
+    }
+
+    private Node _root;
+
+    public BinaryTree() {
+        this._root = null;
+    }
+
+    public BinaryTree(Node root) {
+        this._root = root;
+    }
 
 
 }
