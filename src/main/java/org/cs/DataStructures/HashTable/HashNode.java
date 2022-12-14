51ec23c39e3971d9948bdfbbbe750419fdf5e2a8
ZQR0 <yarikrech@gmail.com>
2022-12-14 14:22:53 +0300

    hash node [docs added, new field]

--- src/main/java/org/cs/DataStructures/HashTable/HashNode.java
+++ src/main/java/org/cs/DataStructures/HashTable/HashNode.java
@@ -7,10 +7,27 @@ package org.cs.DataStructures.HashTable;
 */
 
 public class HashNode<K, V> {
+    /*
+    * @param K _key - key of pair
+    */
     K _key;
+    /*
+    * @param V _value - value for key
+    */
     V _value;
+    /*
+    * @param int _hashCode - just a hash code of params
+    */
     private final int _hashCode;
 
+    /*
+    * @param HashNode<K, V> next - param for getting a next node of table
+    */
+    public HashNode<K, V> next;
+
+    /*
+    * Constructor
+    */
     public HashNode(K key, V value, int hashCode) {
         this._key = key;
         this._value = value;
