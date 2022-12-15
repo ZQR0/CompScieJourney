9ec78557b855dbf48a870c23ba2e057295e44b6c
ZQR0 <yarikrech@gmail.com>
2022-12-15 20:39:44 +0300

    zigzag conversion added

---
+++ src/main/java/org/cs/Leetcode/SigzagConversion.java
@@ -0,0 +1,23 @@
+package org.cs.Leetcode;
+
+public class SigzagConversion {
+    public String convert(String s, int numRows) {
+        StringBuilder sb = new StringBuilder();
+        int interval = (numRows + numRows - 2) == 0 ? 1 : (numRows + numRows - 2);
+
+        for (int r = 0; r < numRows; r++) {
+            // append each character of each row
+            for (int i = r; i < s.length(); i += interval) {
+
+
+                sb.append(s.charAt(i));
+
+                if (r != 0 && r != numRows - 1 && i + interval - r * 2 < s.length()) {
+                    sb.append(s.charAt(i + interval -r * 2));
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
